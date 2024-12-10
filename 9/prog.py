f9f897696d93eebb71b22aac5e9897e5a0a98c89
Nick Bensema <nickbensema@hotmail.com>
2024-12-09 18:11:25 -0700

    finished day 9

--- 9/prog.py
+++ 9/prog.py
@@ -27,24 +27,47 @@ fileId = 0
 
 disk = get_disk(sys.stdin)
 
-def is_complete(disk):
-    return all(sector is None for sector in disk[-disk.count(None):])
-assert is_complete( [2,2,2,None,None] )
-assert not is_complete( [2,None,2,2,None] )
+def puzzle1(indisk):
+    disk=indisk.copy()
+    searchIndex=len(disk)
+    firstNoneSector = 0
+    while True:
+        try:
+            firstNoneSector = disk.index(None, firstNoneSector, searchIndex)
+        except:
+            break
+        searchIndex -= 1
+        if disk[searchIndex] is None:
+            continue
+        disk[firstNoneSector], disk[searchIndex] = disk[searchIndex], disk[firstNoneSector]
+    return disk
 
-searchIndex=len(disk)
-firstNoneSector = 0
-while True:
-    try:
-        firstNoneSector = disk.index(None, firstNoneSector, searchIndex)
-    except:
-        break
-    searchIndex -= 1
-    if disk[searchIndex] is None:
-        continue
-    disk[firstNoneSector], disk[searchIndex] = disk[searchIndex], disk[firstNoneSector]
+def puzzle2(indisk):
+    disk=indisk.copy()
+    # start with highest id
+    maxFileId = max(filter(None,disk))
+    for fileId in range(maxFileId,0,-1):
+        sourceLeft = disk.index(fileId)
+        for sourceRight,sector in enumerate(disk[sourceLeft:],sourceLeft):
+            if sector != fileId:
+                break
+        else: #no blank sectors at end
+            sourceRight += 1 # compensate for edge case
+        sourceLength = sourceRight-sourceLeft
+        destRangeLeft = disk.index(None)
+        if sourceLeft < destRangeLeft:
+            break # no more room to the left of source
+        for destLeft in range(destRangeLeft,sourceLeft):
+            destRight = destLeft+sourceLength
+            if all(sector is None for sector in disk[destLeft:destRight]):
+                # perform swap
+                disk[sourceLeft:sourceRight], disk[destLeft:destRight] = disk[destLeft:destRight], disk[sourceLeft:sourceRight]
+                break # and continue outer loop
+    return disk
 
-#print(disk)
-print(sum(a*b for a,b in enumerate(disk) if b))
+disk1 = puzzle1(disk)
+print(sum(a*b for a,b in enumerate(disk1) if b))
+disk2 = puzzle2(disk)
+print(sum(a*b for a,b in enumerate(disk2) if b))
 
 
